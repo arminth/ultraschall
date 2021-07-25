e30dae96543d0e4ed0718999bf81d070e704bfcc
arminth <armin@a-thinnes.de>
2021-07-25 19:35:31 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,30 @@
 #include <Arduino.h>
+#define settime 2 ;
+#define on_off 3 ;
+#define t60s 4 ;
+#define t120s 5 ;
+#define t180s 6 ;
+#define SSR 7 ;
+
+int timer[3] = {60,120,180}; 
+int zeit = 0;
 
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(t60s, OUTPUT);
+  pinMode(t120s, OUTPUT);
+  pinMode(t180s, OUTPUT);
+  pinMode(on_off, OUTPUT);
+  pinMode(settime, INPUT);
+  pinMode(on_off, INPUT);
+  digitalWrite(SSR,LOW);
+  digitalWrite(on_off,LOW);
+  digitalWrite(t60s,HIGH);
+  digitalWrite(t120s,LOW);
+  digitalWrite(t180s,LOW);
+  
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 }
-//ultraschall
