16254fab4510605824842f61ef8a27e210a060d9
arminth <armin@a-thinnes.de>
2021-07-26 11:49:27 +0200

    Version1.0 final

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,40 @@
+// Controller software for Ultrasonic Cleaner on Arduino Nano
+// Wiring: D4 - LED Green Anode - LED Kathode - GND
+// Wiring: D5 - LED Yellow Anode - LED Kathode - GND
+// Wiring: D6 - LED Red Anode - LED Kathode - GND
+// Wiring: D7 - LED Bluen Anode - LED Kathode - GND and
+// Wiring: D7 - SSR Control_in + 
+// Wiring: SSR Control_-  GND
+// Wiring: D2 - Button 1_in (Settime) - Button_1_out - GND
+// Wiring: D3 - Button 2_in (ON/OFF) - Button_2_out - GND 
+
+#define FASTLED_INTERNAL        // Suppress build banner
 #include <Arduino.h>
+#include <FastLED.h>
+
 #define settime 2  //Input button 1
 #define on_off 3  // Input button 2
-#define t60s 4  // LED Blue
-#define t120s 5 // LED Green
-#define t180s 6  // LED Yellow
-#define SSR 7  //Output to control SSR
+#define t60s 4  // LED Green
+#define t120s 5 // LED Yellow
+#define t180s 6  // LED Red
+#define SSR 7  //Output to control SSR and LED Blue
 
-unsigned long time[3] = {60000,120000,180000};
-int timerstate = 0; 
+unsigned long time[3] = {60000,120000,180000}; // Timer-array, time in milliseconds
 int zeit = 0;
 int State_on_off = LOW;         // the current state of the output pin
 int button_on_offState;             // the current reading from the input pin
 int button_timerState;
-int lastButton_on_off_State = LOW;   // the previous reading from the input pin
-int lastButton_timer_State = LOW;   // the previous reading from the input pin
+int lastButton_on_off_State = HIGH;   // the previous reading from the input pin
+int lastButton_timer_State = HIGH;   // the previous reading from the input pin
+ int onoffbutton;
+ int timerbutton;
 
 // the following variables are unsigned longs because the time, measured in
 // milliseconds, will quickly become a bigger number than can be stored in an int.
 unsigned long lastDebounceTime1 = 0;  // the last time the on_off pin was toggled
 unsigned long lastDebounceTime2 = 0;  // the last time the settime pin was toggled
 unsigned long startTime = 0; //When did we push start
-unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
+unsigned long debounceDelay = 30;    // the debounce time; increase if the output flickers
 
 void setup() {
   pinMode(t60s, OUTPUT);
@@ -34,14 +48,17 @@ void setup() {
   digitalWrite(t60s,HIGH);
   digitalWrite(t120s,LOW);
   digitalWrite(t180s,LOW);
-  
-
+   Serial.begin(115200); 
+ onoffbutton = digitalRead(on_off);
+ timerbutton = digitalRead(settime);
 }
 
 void loop() {
-
-  int onoffbutton = digitalRead(on_off);
-
+  lastButton_on_off_State = onoffbutton;
+  lastButton_timer_State = timerbutton;
+   onoffbutton = digitalRead(on_off);
+  
+ 
   // check to see if you just pressed the ON/OFF button
   // (i.e. the input went from LOW to HIGH), and you've waited long enough
   // since the last press to ignore any noise:
@@ -50,27 +67,33 @@ void loop() {
   if (onoffbutton != lastButton_on_off_State) {
     // reset the debouncing timer
     lastDebounceTime1 = millis();
+    
+    delay (50);
   }
 
   if ((millis() - lastDebounceTime1) > debounceDelay) {
     // whatever the reading is at, it's been there for longer than the debounce
     // delay, so take it as the actual current state:
-
-    // if the button state has changed:
-    if (onoffbutton != button_on_offState) {
+     
+    // if the onoffbutton state has changed from high to low:
+    if ((onoffbutton != lastButton_on_off_State) && onoffbutton==LOW) {
       button_on_offState = onoffbutton;
+      
 
-      // only toggle the LED if the new button state is HIGH
-      if (button_on_offState == HIGH) {
+      // only toggle the LED if the new button state is LOW
+      if (button_on_offState == LOW) {
         State_on_off = !State_on_off;
         startTime = millis();
+        Serial.print("Toggel change ON/OFF................................................... ");Serial.println(State_on_off);
       }
     }
   }
 
+
+
   if (State_on_off != HIGH){
      digitalWrite(SSR,LOW);
-    int timerbutton = digitalRead(settime);
+     timerbutton = digitalRead(settime);
 
   // check to see if you just pressed the settime-button
   // (i.e. the input went from LOW to HIGH), and you've waited long enough
@@ -80,20 +103,23 @@ void loop() {
   if (timerbutton != lastButton_timer_State) {
     // reset the debouncing timer
     lastDebounceTime2 = millis();
+    delay(50);
   }
 
   if ((millis() - lastDebounceTime2) > debounceDelay) {
     // whatever the reading is at, it's been there for longer than the debounce
     // delay, so take it as the actual current state:
 
-    // if the button state has changed:
-    if (timerbutton != button_timerState) {
+    // if the timerbutton state has changed:
+    if ((timerbutton != lastButton_timer_State) && timerbutton==LOW){
       button_timerState = timerbutton;
 
-      // only toggle the LED if the new button state is HIGH
-      if (button_timerState == HIGH) {
+      // only toggle the LED if the new button state is LOW
+      if (button_timerState == LOW) {
+        
         zeit ++;
         if (zeit == 3) {zeit = 0;};
+        Serial.print("Toggel change Time................................................... ");Serial.println(time[zeit]);
       } 
         switch (zeit){
           case 0:
@@ -125,7 +151,10 @@ void loop() {
     }
   } else {
     
-    if ((millis() - startTime) < time[zeit]) { digitalWrite(SSR,HIGH);} else {digitalWrite(SSR,LOW);};
+    if ((millis() - startTime) <= time[zeit]) {EVERY_N_MILLISECONDS(10000) {Serial.print("S");}; digitalWrite(SSR,State_on_off);} else {Serial.println(""); 
+    State_on_off = !State_on_off; digitalWrite(SSR,State_on_off);
+    Serial.print("Toggel change ON/OFF................................................... ");Serial.println(State_on_off);
+    };
 
   }
 
